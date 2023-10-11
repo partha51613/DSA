cde7d0115749ab470fd58f188350357bf8f280d4
Partha <github@partha51613.com>
2023-10-11 19:52:30 +0530

    Pattern2

---
+++ others/Pattern2.java
@@ -0,0 +1,34 @@
+
+/**
+ * Pattern 
+ * 
+ * 
+ * *****
+ * ****
+ * ***
+ * **
+ * *
+ * 
+ * 
+ */
+
+
+public class Pattern2 {
+    public static void main(String[] args){
+        
+        int i=0;
+        int j=0;
+        for(j=0;j<5;j++){
+            for(i=j;i<5;i++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
+
+
+
+
