a1d2abc096a26378f39a059a3ff5990294f6bdb0
Partha <github@partha51613.com>
2023-10-11 19:51:17 +0530

    Patter1

--- others/Pattern1.java
+++ others/Pattern1.java
@@ -2,6 +2,14 @@
 
 public class Pattern1 {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        
+        int i=0;
+        int j=0;
+        for(j=0;j<5;j++){
+            for(i=0;i<5;i++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
