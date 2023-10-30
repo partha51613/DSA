da62d173c77396c744665274f20a92d5a2b24b3e
Partha <github@partha51613.com>
2023-10-30 19:42:59 +0530

    KadaneAlgorithm for max subarray sum

--- arrays/medium/KadaneAlgo.java
+++ arrays/medium/KadaneAlgo.java
@@ -8,22 +8,44 @@ public class KadaneAlgo {
         System.out.println("Sumarray sum: " +subArraySum(arr));
         }
 
+        
+
+        /** O(N) solution 
+         * Optimised solution
+        */
+
         public static int subArraySum(int[] arr){
-            int maxSum=0;
+            int n = arr.length;
             int sum=0;
+            int maxSum = Integer.MIN_VALUE;
 
-            int i=0;
-            int j=0;
-            int n=arr.length;
-
-            for(i=0;i<n;i++){
-                sum=0;
-                for(j=i;j<n;j++){
-                    sum+=arr[j];
-                    maxSum = Math.max(maxSum,sum);
-                }
+            for(int i=0;i<n;i++){
+                sum+= arr[i];
+                maxSum = Math.max(maxSum, sum);
+                if(sum<0) sum=0;
             }
-            
             return maxSum;
         }
+
+    
+        /* O(N^2) */
+
+        // public static int subArraySum(int[] arr){
+        //     int maxSum=0;
+        //     int sum=0;
+
+        //     int i=0;
+        //     int j=0;
+        //     int n=arr.length;
+
+        //     for(i=0;i<n;i++){
+        //         sum=0;
+        //         for(j=i;j<n;j++){
+        //             sum+=arr[j];
+        //             maxSum = Math.max(maxSum,sum);
+        //         }
+        //     }
+            
+        //     return maxSum;
+        // }
 }
